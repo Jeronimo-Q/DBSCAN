c463b5a426226a6348e8eecbc26ce804f3824d3b
juanes <juanest006@gmail.com>
2025-11-17 17:59:42 -0500

    Grafico con plotly

--- grafico.py
+++ grafico.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 
 def generateHeatmap(Data):
@@ -8,13 +9,43 @@ def generateHeatmap(Data):
     plt.title("Mapa de calor de densidad de crímenes")
     plt.xlabel("Longitud")
     plt.ylabel("Latitud")
+    plt.savefig("results/heatmap_crimenes.png")
     plt.show()
+
+
+def generateHeatmapPlotly(Data):
+
+    fig = px.density_mapbox(
+        Data,
+        lat="Latitude",
+        lon="Longitude",
+        radius=10,                
+        center=dict(lat=51.42, lon=-2.4),
+        zoom=9,
+        mapbox_style="open-street-map",  
+        color_continuous_scale="Reds"
+)
+
+    fig.update_layout(
+        title="Mapa de calor de crímenes en Londres"
+    )
+
+    fig.show()
     
 def generateDiagramaBarras(data):
     crime_counts = data['Crime type'].value_counts().head(10)
+
+    norm = plt.Normalize(crime_counts.min(), crime_counts.max())
+    colors = plt.cm.viridis(norm(crime_counts.values))
+
     plt.figure(figsize=(10,6))
-    sns.barplot(x=crime_counts.values, y=crime_counts.index, palette="viridis")
+    sns.barplot(
+        x=crime_counts.values,
+        y=crime_counts.index,
+        palette=colors
+    )
     plt.title("Top 10 tipos de crímenes")
     plt.xlabel("Número de incidentes")
     plt.ylabel("Tipo de crimen")
     plt.show()
+    plt.savefig("results/diagrama_barras_crimenes.png")
